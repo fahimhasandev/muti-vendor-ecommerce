3d5ba2e79f4a757d36fe2c2d97e2ba9e121b8fd0
fahimhasandev <fhasan5@wgu.edu>
2024-12-05 15:16:16 -0600

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function Home() {
   return (
-    <div>
-      <p className='text-rose-500'>Hello world</p>
-      <Button variant='destructive'>Hello World</Button>
+    <div className='p-4'>
+      <div className='flex flex-col gap-y-4'>
+        <div>
+          <Button variant='elevated'>I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder='I am an Input'></Input>
+        </div>
+        <div>
+          <Progress value={90} />
+        </div>
+        <div>
+          <Textarea></Textarea>
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
